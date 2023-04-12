ae9d7f06b2e0672f3b5213bd21f57ebc85a3c979
Jean-Baptiste Lievremont <mithfindel@gmail.com>
2023-04-12 14:03:49 +0200

    Add first function with test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, I'm Crabby! 🦀");
+    println!("Life, the universe + the rest = {}", add(40, 2));
+}
+
+fn add(a: u16, b: u16 ) -> u16 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::add;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(12, 5), 17);
+    }
+
+    #[test]
+    fn test_add_large() {
+        assert_eq!(add(255, 1), 256);
+    }
 }
