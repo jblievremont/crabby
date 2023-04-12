36e42d87493dcffecaf47ba229f0fc42bf4ac78d
Jean-Baptiste Lievremont <mithfindel@gmail.com>
2023-04-12 14:58:31 +0200

    Use command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,51 @@
+use std::env;
+
 fn main() {
-    // define your games a and b
-    let a = Game::Rock;
-    let b = Game::Scissor;
-    // call play function with arguments
-    let result = play(&a, &b);
-    // display result
-    println!("Result of playing {:?} vs {:?} is {:?}", a, b, result);
+    let args: Vec<String> = env::args().collect();
+
+    let cmd = args.get(1).expect("Please provide a command in (greet, chifoumi)");
+
+    match cmd.as_str() {
+        "greet" => {
+            let name = args.get(2).expect("Name is required for greetings");
+            println!("{}", greets(&name));
+        }
+        "chifoumi" => {
+            let p_one = args.get(2).expect("Player one is required for chifoumi");
+            let p_two = args.get(3).expect("Player two is required for chifoumi");
+            // cast to Game enum
+            let p_one: Game = Game::try_from(p_one).unwrap();
+            let p_two: Game = Game::try_from(p_two).unwrap();
+
+            let result = play(&p_one, &p_two);
+            println!("p1 vs p2 : {:?}", result);
+        }
+        _ => eprintln!("Not supported command"),
+    }
+}
+
+impl TryFrom<&String> for Game {
+    type Error = String;
+
+    fn try_from(s: &String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "rock" => Ok(Game::Rock),
+            "paper" => Ok(Game::Paper),
+            "scissors" => Ok(Game::Scissors),
+            _ => Err("Invalid game".to_string()),
+        }
+    }
+}
+
+fn greets(who: &str) -> String {
+    format!("Hello, {who}!")
 }
 
 #[derive(Debug)]
 enum Game {
     Rock,
     Paper,
-    Scissor
+    Scissors
 }
 
 #[derive(Debug, PartialEq)]
@@ -24,9 +57,9 @@ enum GameResult {
 
 fn play(a: &Game, b: &Game) -> GameResult {
     match(a, b) {
-        (Game::Rock, Game::Rock) | (Game::Paper, Game::Paper) | (Game::Scissor, Game::Scissor) => GameResult::Draw,
-        (Game::Rock, Game::Scissor) | (Game::Paper, Game::Rock) | (Game::Scissor, Game::Paper) => GameResult::Win,
-        (Game::Rock, Game::Paper) | (Game::Paper, Game::Scissor) | (Game::Scissor, Game::Rock) => GameResult::Lost
+        (Game::Rock, Game::Rock) | (Game::Paper, Game::Paper) | (Game::Scissors, Game::Scissors) => GameResult::Draw,
+        (Game::Rock, Game::Scissors) | (Game::Paper, Game::Rock) | (Game::Scissors, Game::Paper) => GameResult::Win,
+        (Game::Rock, Game::Paper) | (Game::Paper, Game::Scissors) | (Game::Scissors, Game::Rock) => GameResult::Lost
     }
 } 
 
@@ -51,12 +84,12 @@ mod tests {
     play_tests! {
         play_r_r_d: (Game::Rock, Game::Rock, GameResult::Draw),
         play_p_p_d: (Game::Paper, Game::Paper, GameResult::Draw),
-        play_s_s_d: (Game::Scissor, Game::Scissor, GameResult::Draw),
-        play_r_s_w: (Game::Rock, Game::Scissor, GameResult::Win),
+        play_s_s_d: (Game::Scissors, Game::Scissors, GameResult::Draw),
+        play_r_s_w: (Game::Rock, Game::Scissors, GameResult::Win),
         play_p_r_w: (Game::Paper, Game::Rock, GameResult::Win),
-        play_s_p_w: (Game::Scissor, Game::Paper, GameResult::Win),
+        play_s_p_w: (Game::Scissors, Game::Paper, GameResult::Win),
         play_r_p_l: (Game::Rock, Game::Paper, GameResult::Lost),
-        play_p_s_l: (Game::Paper, Game::Scissor, GameResult::Lost),
-        play_s_r_l: (Game::Scissor, Game::Rock, GameResult::Lost),
+        play_p_s_l: (Game::Paper, Game::Scissors, GameResult::Lost),
+        play_s_r_l: (Game::Scissors, Game::Rock, GameResult::Lost),
     }
 }
